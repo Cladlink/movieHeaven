80c6db36aa52f05aff8c5983420f79123fb42f3f
MLucile <mlucile.08@gmail.com>
2016-11-29 19:23:26 +0100

    Ajout du visuel du panier (a vérifier) mais je vais manger donc je continue après :)

--- src/AppBundle/Controller/BoutiqueController.php
+++ src/AppBundle/Controller/BoutiqueController.php
@@ -9,7 +9,20 @@
 namespace AppBundle\Controller;
 
 
-class BoutiqueController
-{
+use AppBundle\Entity\Utilisateur;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
 
+class BoutiqueController extends Controller
+{
+    /**
+     * @Route("/panier", name="afficherPanier")
+     */
+    public function afficherPanier(Utilisateur $utilisateur)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $contenu = $em->getRepository('AppBundle:Panier')
+            ->findBy(array('utilisateurId' => $utilisateur->getIdUtilisateur()));
+        return $this->render('admin/gestionFilms.html.twig', (['contenu' => $contenu]));
+    }
 }
