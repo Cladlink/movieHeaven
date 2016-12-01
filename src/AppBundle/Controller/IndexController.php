a3615377ca856679c110045a8e0760d546262cfb
MLucile <mlucile.08@gmail.com>
2016-12-01 21:57:37 +0100

    Date limite pour le panier : le panier est vidé si l'utilisateur se log trop tard

--- src/AppBundle/Controller/IndexController.php
+++ src/AppBundle/Controller/IndexController.php
@@ -19,6 +19,40 @@ class IndexController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('index.html.twig');
+        $utilisateur = $this->getUser();
+        $erreur = false;
+        if($utilisateur != null)
+        {
+            $em = $this->getDoctrine()->getManager();
+            $etat = $em->getRepository('AppBundle:EtatCommande')->findOneBy(['libelleEtatCommande' => 'Pas commandee']);
+            $commandeEnCours = $em->getRepository('AppBundle:Commande')->findOneBy([
+                'utilisateurId' => $utilisateur,
+                'etatId' => $etat
+            ]);
+            if($commandeEnCours != null)
+            {
+                $dateActuelle = new \DateTime(date('Y-m-d'));
+                $datePanier = $commandeEnCours->getDateCommande();
+                $dateButoir = (new \DateTime(($datePanier->format('Y-m-d')).'+5 days'));
+                $diff = $dateButoir->diff($dateActuelle);
+                $diffString = $diff->format('%R%a days');
+                if(str_split($diffString)[0] == '+')
+                {
+                    $paniersConcernes = $em->getRepository('AppBundle:Panier')->findBy([
+                        'commandeId' => $commandeEnCours
+                    ]);
+                    foreach ($paniersConcernes as $key => $panier)
+                    {
+                        $em->remove($panier);
+                        $em->flush();
+                    }
+                    $em->remove($commandeEnCours);
+                    $em->flush();
+                    $erreur = "Votre panier a ete supprime car cela faisait trop longtemps que vous n etes pas venu.";
+                }
+
+            }
+        }
+        return $this->render('index.html.twig', (['erreur' => $erreur]));
     }
 }
